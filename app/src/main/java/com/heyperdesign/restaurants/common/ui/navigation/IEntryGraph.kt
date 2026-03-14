9e8f0ba8e5b63724e18a8d5b3680072557a674ea
Rodinamomen <Rodinamobark3@gmail.com>
2026-03-15 00:15:39 +0200

    wip : add onboarding destination and set as start destination in entry graph

--- app/src/main/java/com/heyperdesign/restaurants/common/ui/navigation/IEntryGraph.kt
+++ app/src/main/java/com/heyperdesign/restaurants/common/ui/navigation/IEntryGraph.kt
@@ -1,7 +1,9 @@
 package com.heyperdesign.restaurants.common.ui.navigation
 
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.heyperdesign.restaurants.feature.entry.onboarding.ui.view.OnBoardingScreen
 import kotlinx.serialization.Serializable
 
 sealed interface IEntryGraph {
@@ -10,10 +12,13 @@ sealed interface IEntryGraph {
 
     @Serializable
     data object Splash : IDestination
+
+    @Serializable
+    data object OnBoarding : IDestination
 }
 
 fun NavGraphBuilder.buildNavEntryGraph() {
-    navigation<IEntryGraph.RootGraph>(startDestination = IEntryGraph.Splash) {
-
+    navigation<IEntryGraph.RootGraph>(startDestination = IEntryGraph.OnBoarding) {
+        composable<IEntryGraph.OnBoarding> { OnBoardingScreen() }
     }
 }
