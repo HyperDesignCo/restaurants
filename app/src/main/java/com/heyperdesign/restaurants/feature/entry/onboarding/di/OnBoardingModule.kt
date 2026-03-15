ea1de60e3b89ed02a336efe822ad1dfacf33b10e
Rodinamomen <Rodinamobark3@gmail.com>
2026-03-15 02:21:51 +0200

    wip : add onboarding di dependencies

--- app/src/main/java/com/heyperdesign/restaurants/feature/entry/onboarding/di/OnBoardingModule.kt
+++ app/src/main/java/com/heyperdesign/restaurants/feature/entry/onboarding/di/OnBoardingModule.kt
@@ -1,9 +1,20 @@
 package com.heyperdesign.restaurants.feature.entry.onboarding.di
 
+import com.heyperdesign.restaurants.feature.entry.onboarding.data.repository.OnBoardingRepository
+import com.heyperdesign.restaurants.feature.entry.onboarding.data.repository.local.OnBoardingLocalDataSourceProvider
+import com.heyperdesign.restaurants.feature.entry.onboarding.domain.repository.IOnBoardingRepository
+import com.heyperdesign.restaurants.feature.entry.onboarding.domain.repository.local.IOnboardingLocalDataSourceProvider
+import com.heyperdesign.restaurants.feature.entry.onboarding.domain.usecase.SaveOnBoardingUC
 import com.heyperdesign.restaurants.feature.entry.onboarding.ui.viewmodel.OnBoardingViewModel
+import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val onBoardingModule = module {
+    singleOf(::OnBoardingLocalDataSourceProvider) bind IOnboardingLocalDataSourceProvider::class
+    singleOf(::OnBoardingRepository) bind IOnBoardingRepository::class
+    factoryOf(::SaveOnBoardingUC)
     viewModelOf(::OnBoardingViewModel)
 }
