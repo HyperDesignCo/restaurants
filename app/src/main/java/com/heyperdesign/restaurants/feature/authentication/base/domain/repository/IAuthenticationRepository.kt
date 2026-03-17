29f2441f36f297651352bcde1a96c99ef665ac8c
Rodinamomen <Rodinamobark3@gmail.com>
2026-03-17 15:51:59 +0200

    wip : add signup to authentication repository and remote data source

--- app/src/main/java/com/heyperdesign/restaurants/feature/authentication/base/domain/repository/IAuthenticationRepository.kt
+++ app/src/main/java/com/heyperdesign/restaurants/feature/authentication/base/domain/repository/IAuthenticationRepository.kt
@@ -2,7 +2,9 @@ package com.heyperdesign.restaurants.feature.authentication.base.domain.reposito
 
 import com.heyperdesign.restaurants.feature.authentication.base.domain.model.Authentication
 import com.heyperdesign.restaurants.feature.authentication.login.domain.model.request.LoginRequest
+import com.heyperdesign.restaurants.feature.authentication.signup.domain.model.request.SignupRequest
 
 interface IAuthenticationRepository {
     suspend fun login(request: LoginRequest) : Authentication
+    suspend fun signup(request: SignupRequest) : Authentication
 }
--- app/src/main/java/com/heyperdesign/restaurants/feature/authentication/base/domain/repository/remote/IAuthenticationRemoteDataSource.kt
+++ app/src/main/java/com/heyperdesign/restaurants/feature/authentication/base/domain/repository/remote/IAuthenticationRemoteDataSource.kt
@@ -2,7 +2,9 @@ package com.heyperdesign.restaurants.feature.authentication.base.domain.reposito
 
 import com.heyperdesign.restaurants.feature.authentication.base.data.model.dto.AuthenticationDto
 import com.heyperdesign.restaurants.feature.authentication.login.domain.model.request.LoginRequest
+import com.heyperdesign.restaurants.feature.authentication.signup.domain.model.request.SignupRequest
 
 interface IAuthenticationRemoteDataSource {
     suspend fun login(request : LoginRequest) : AuthenticationDto
+    suspend fun signup(request : SignupRequest) : AuthenticationDto
 }
